c3db8056f768c864532a51a95adc4b2d08337776
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-06-05 16:00:24 +0200

    componenti nestati

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  components: {
+   
+  }
+}
+</script>
+
+<template>
+    <div>
+       <div class="row">
+         <div class="col-4">
+           <div class="card">
+             <div class="card-body">
+               <h3 class="card-title">Title</h3>
+               <p class="card-text">Text</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-4">
+           <div class="card">
+             <div class="card-body">
+               <h3 class="card-title">Title</h3>
+               <p class="card-text">Text</p>
+             </div>
+           </div>
+         </div>
+       </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,29 @@
+<script>
+import Card from './Card.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<template>
+    <div>
+        <div class="dropdown open">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                        Dropdown Button
+                    </button>
+            <div class="dropdown-menu" aria-labelledby="triggerId">
+                <button class="dropdown-item" href="#">Action</button>
+                <button class="dropdown-item disabled" href="#">Disabled action</button>
+            </div>
+        </div>
+       <Card />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
