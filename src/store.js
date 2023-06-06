1c44f3058a6641ff1a54e5400de7ddedc59691c4
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-06-06 17:45:41 +0200

    push corretto

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiURL: " https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Dark Magician ",
+    apiURL: " https://db.ygoprodeck.com/api/v7/cardinfo.php ",
     filterURL: " https://db.ygoprodeck.com/api/v7/archetypes.php ",
     cardList: [],
     filter: [],
